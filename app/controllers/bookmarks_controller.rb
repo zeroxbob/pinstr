ad3fae95b7d2877120d7e2a6c2f9827ba8b2fadd
zeroxbob <zeroxbob@protonmail.com>
2025-04-19 16:40:44 +0200

    Persist more fields with bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,30 +1,38 @@
 class BookmarksController < ApplicationController
   protect_from_forgery with: :exception
 
+  def index
+    @bookmarks = Bookmark.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @bookmarks }
+    end
+  end
+
   def create
     signed_event = params[:signed_event]
 
-    # Extract necessary fields from the signed event and map them
     bookmark_params = {
-      title: signed_event['content'], # Assuming title is in 'content'
-      url: request.referer || 'unknown', # Adjust as needed
+      title: signed_event['content'],
+      url: request.referer || 'unknown',
       description: '',
       event_id: signed_event['id'],
-      user_id: User.find_by(public_key: signed_event['pubkey'])&.id
+      user_id: User.find_by(public_key: signed_event['pubkey'])&.id,
+      signed_event_content: signed_event.to_json,  # Save entire JSON
+      signed_event_sig: signed_event['sig']        # Save signature
     }
 
     @bookmark = Bookmark.new(bookmark_params)
 
     if @bookmark.save
-      render json: { status: 'success', message: 'Bookmark was successfully created and sent to Nostr.' }
+      redirect_to bookmarks_path, notice: 'Bookmark was successfully created and signed event stored.'
     else
-      render json: { status: 'error', message: @bookmark.errors.full_messages.join(', ') }, status: :unprocessable_entity
+      render :new, alert: @bookmark.errors.full_messages.join(', ')
     end
   end
 
-  private
-
-  def bookmark_params
-    params.require(:bookmark).permit(:title, :url, :description, :event_id, :user_id)
+  def new
+    @bookmark = Bookmark.new
   end
 end
