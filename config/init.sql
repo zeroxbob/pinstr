4e94c704603d8ad67afb96b23dc9cd4b20c9cd06
zeroxbob <zeroxbob@protonmail.com>
2025-06-27 17:33:37 +0200

    Create DBs for cache, queue and cable

--- config/init.sql
+++ config/init.sql
@@ -1,1 +1,4 @@
-CREATE DATABASE pinstr_production;
+CREATE DATABASE IF NOT EXISTS `pinstr_production`;
+CREATE DATABASE IF NOT EXISTS `pinstr_production_cache`;
+CREATE DATABASE IF NOT EXISTS `pinstr_production_queue`;
+CREATE DATABASE IF NOT EXISTS `pinstr_production_cable`;
